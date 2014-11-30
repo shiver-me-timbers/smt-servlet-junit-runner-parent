7e078f3265e20f4702a651388d8dd27eb72081f0
karlbennett <where_1ts@yahoo.com>
2014-11-30 22:46:56 +1300

    Fixed the JavaDocs in AnnotationServletJUnitRunner.

--- smt-servlet-junit-runner/src/main/java/shiver/me/timbers/junit/runner/servlet/AnnotationServletJUnitRunner.java
+++ smt-servlet-junit-runner/src/main/java/shiver/me/timbers/junit/runner/servlet/AnnotationServletJUnitRunner.java
@@ -12,20 +12,20 @@ import shiver.me.timbers.junit.runner.servlet.config.SettablePortConfigFactory;
  * Adding this runner to a JUnit class will cause a servlet server to start up before the test.
  *
  * The server will start up on a random free port which can be accessed within the test by annotating an
- * {@code Integer}/{@code int} field with {@link shiver.me.timbers.junit.runner.servlet.annotation.Port}. The port number can
- * be set manually by instead annotating the test class with with {@code Port} and setting it's value to the desired
- * port number.
+ * {@code Integer}/{@code int} field with {@link shiver.me.timbers.junit.runner.servlet.annotation.Port}. The port
+ * number can be set manually by instead annotating the test class with with {@code Port} and setting it's value to the
+ * desired port number.
  *
  * By default the started server will scan the package of the test file for any classes annotated with
  * {@link javax.servlet.annotation.WebServlet} which it will then load.
  *
- * Alternatively the {@link shiver.me.timbers.junit.runner.servlet.annotation.Servlets} annotation can be used to restrict which
- * servlet classes are loaded.
+ * Alternatively the {@link shiver.me.timbers.junit.runner.servlet.annotation.Servlets} annotation can be used to
+ * restrict which servlet classes are loaded.
  *
  * The server can be configured for an individual test class with a method that has the servlet containers configuration
  * object as it's first argument and has been annotated with
- * {@link shiver.me.timbers.junit.runner.servlet.annotation.ContainerConfig}. This will cause a new server instance to start up
- * for that specific test class.
+ * {@link shiver.me.timbers.junit.runner.servlet.annotation.ContainerConfig}. This will cause a new server instance to
+ * start up for that specific test class.
  *
  * If the same configuration can be used across multiple test classes then the classes can be annotated with
  * {@code ContainerConfig} that has it's value set to an implementation of
