6844546a42a4579eb492ba77f937ce8dd8b91afb
Karl Bennett <where_1ts@yahoo.com>
2015-01-26 13:45:12 +1300

    Improved the ContainsAllMatcher so that it no longer gives a false positive for lists with duplicate elements.

--- smt-servlet-junit-runner/src/test/java/shiver/me/timbers/junit/runner/servlet/test/ContainsAllMatcher.java
+++ smt-servlet-junit-runner/src/test/java/shiver/me/timbers/junit/runner/servlet/test/ContainsAllMatcher.java
@@ -3,6 +3,9 @@ package shiver.me.timbers.junit.runner.servlet.test;
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * This matcher can be used to check that two {@link Iterable}s contain the same elements. The order of the elements is
  * ignored.
@@ -20,16 +23,21 @@ public class ContainsAllMatcher<I extends Iterable> extends TypeSafeMatcher<I> {
     @Override
     protected boolean matchesSafely(I actual) {
 
+        final List<Object> processingActual = toList(actual);
+
         for (Object exp : expected) {
-            if (!matchFound(actual, exp)) {
+
+            if (!matchFound(processingActual, exp)) {
                 return false;
             }
+
+            processingActual.remove(exp);
         }
 
         return true;
     }
 
-    private boolean matchFound(I actual, Object exp) {
+    private boolean matchFound(List<Object> actual, Object exp) {
 
         for (Object act : actual) {
             if (exp.equals(act)) {
@@ -43,5 +51,16 @@ public class ContainsAllMatcher<I extends Iterable> extends TypeSafeMatcher<I> {
     @Override
     public void describeTo(Description description) {
         description.appendValue(expected);
+    }
+
+    private static List<Object> toList(Iterable expected) {
+
+        final List<Object> list = new LinkedList<>();
+
+        for (Object element : expected) {
+            list.add(element);
+        }
+
+        return list;
     }
 }
