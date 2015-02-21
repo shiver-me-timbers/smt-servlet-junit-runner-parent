fc86100ccea7e628fd49f2cd314142fd7a1976b4
Karl Bennett <where_1ts@yahoo.com>
2015-02-21 17:01:11 +1300

    Created a new smt-tomcat-junit-runner project that will contain all the common logic shared by the version specific Tomcat projects.

---
+++ smt-tomcat-junit-runner-parent/smt-tomcat-junit-runner/src/main/java/shiver/me/timbers/junit/runner/tomcat/filter/FilterDetailFilterDef.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2015  Karl Bennett
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package shiver.me.timbers.junit.runner.tomcat.filter;
+
+import org.apache.catalina.deploy.FilterDef;
+import shiver.me.timbers.junit.runner.servlet.FilterDetail;
+
+import static java.util.Map.Entry;
+
+/**
+ * @author Karl Bennett
+ */
+public class FilterDetailFilterDef extends FilterDef {
+
+    public FilterDetailFilterDef(FilterDetail filterDetail) {
+
+        setFilter(filterDetail.getFilterInstance());
+        setDescription(filterDetail.getDescription());
+        setDisplayName(filterDetail.getDisplayName());
+        setFilterName(filterDetail.getFilterName());
+        setSmallIcon(filterDetail.getSmallIcon());
+        setLargeIcon(filterDetail.getLargeIcon());
+        setAsyncSupported(String.valueOf(filterDetail.asyncSupported()));
+
+        for (Entry<String, String> initParam : filterDetail.getInitParams().entrySet()) {
+            addInitParameter(initParam.getKey(), initParam.getValue());
+        }
+    }
+}
---
+++ smt-tomcat-junit-runner-parent/smt-tomcat-junit-runner/src/main/java/shiver/me/timbers/junit/runner/tomcat/filter/FilterDetailFilterMap.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2015  Karl Bennett
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package shiver.me.timbers.junit.runner.tomcat.filter;
+
+import org.apache.catalina.deploy.FilterMap;
+import shiver.me.timbers.junit.runner.servlet.FilterDetail;
+
+import javax.servlet.DispatcherType;
+
+/**
+ * @author Karl Bennett
+ */
+public class FilterDetailFilterMap extends FilterMap {
+
+    public FilterDetailFilterMap(FilterDetail filterDetail) {
+
+        setFilterName(filterDetail.getFilterName());
+
+        for (String servletName : filterDetail.getServletNames()) {
+            addServletName(servletName);
+        }
+
+        for (String urlPattern : filterDetail.getUrlPatterns()) {
+            addURLPattern(urlPattern);
+        }
+
+        // Each call to FilterMap.setDispatcher(String) with a different DispatcherType actually sets a bit flag for
+        // each type.
+        for (DispatcherType dispatcherType : filterDetail.getDispatcherTypes()) {
+            setDispatcher(dispatcherType.name());
+        }
+    }
+}
