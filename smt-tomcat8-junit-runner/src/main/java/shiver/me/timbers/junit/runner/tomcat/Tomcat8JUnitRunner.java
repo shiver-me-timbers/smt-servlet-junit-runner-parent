f1cb18262d9b9fa24bf6e3a6821560de8d693e0c
Karl Bennett <where_1ts@yahoo.com>
2015-02-21 16:36:18 +1300

    Quick hack copy of smt-tomcat7-junit-runner to add support for Tomcat 8.

---
+++ smt-tomcat8-junit-runner/src/main/java/shiver/me/timbers/junit/runner/tomcat/Tomcat8JUnitRunner.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2015  Karl Bennett
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package shiver.me.timbers.junit.runner.tomcat;
+
+import org.apache.catalina.startup.Tomcat;
+import org.junit.runners.model.InitializationError;
+import shiver.me.timbers.junit.runner.servlet.AnnotationServletJUnitRunner;
+
+import javax.servlet.ServletException;
+
+/**
+ * This JUnit test runner will start up an Embedded Tomcat instance for the related test. The
+ * {@link shiver.me.timbers.junit.runner.servlet.annotation.ContainerConfiguration} annotation can then be used to
+ * configure the Tomcat instance.
+ *
+ * @author Karl Bennett
+ */
+public class Tomcat8JUnitRunner extends AnnotationServletJUnitRunner<Tomcat> {
+
+    public Tomcat8JUnitRunner(Class test) throws InitializationError, ServletException {
+        super(new Tomcat8Container(new Tomcat()), test);
+    }
+}
---
+++ smt-tomcat8-junit-runner/src/test/java/shiver/me/timbers/junit/runner/tomcat/Tomcat8ContainerTest.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright (C) 2015  Karl Bennett
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package shiver.me.timbers.junit.runner.tomcat;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static shiver.me.timbers.junit.runner.tomcat.Tomcat8Container.copy;
+import static shiver.me.timbers.junit.runner.tomcat.Tomcat8Container.copyAndClose;
+import static shiver.me.timbers.junit.runner.tomcat.Tomcat8Container.tempWebXml;
+import static shiver.me.timbers.junit.runner.tomcat.Tomcat8Container.withException;
+
+public class Tomcat8ContainerTest {
+
+    public static final URL WEB_XML_JAR = Thread.currentThread().getContextClassLoader().getResource("web.xml");
+
+    @Test
+    public void It_is_possible_to_run_with_a_life_cycle() {
+
+        // Given
+        final Callable<Boolean> lifeCycle = new Callable<Boolean>() {
+            @Override
+            public Boolean call() {
+                return true;
+            }
+        };
+
+        // When
+        final Boolean actual = withException(lifeCycle);
+
+        // Then
+        assertTrue(actual);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void It_is_possible_to_run_with_a_life_cycle_and_have_it_fail() {
+
+        // Given
+        final Callable<Void> lifeCycle = new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                throw new Exception();
+            }
+        };
+
+        // When
+        withException(lifeCycle);
+    }
+
+    @Test
+    public void It_is_possible_to_create_a_temporary_web_xml() throws IOException {
+
+        // Given
+        final int hash = new Random().nextInt();
+
+        // When
+        final File webXml = tempWebXml(hash);
+
+        // Then
+        assertTrue(webXml.createNewFile());
+    }
+
+    @Test
+    public void It_is_possible_to_copy_and_close_a_jar_web_xml_to_the_filesystem() throws IOException {
+
+        // Given
+        final int hash = new Random().nextInt();
+        final File webXml = tempWebXml(hash);
+        final InputStream input = WEB_XML_JAR.openStream();
+        final OutputStream output = new FileOutputStream(webXml);
+
+        // When
+        copyAndClose(input, output);
+
+        // Then
+        assertTrue(webXml.exists());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void It_is_not_possible_to_copy_and_close_from_an_invalid_input() throws IOException {
+
+        final InputStream input = mock(InputStream.class);
+        final OutputStream output = mock(OutputStream.class);
+
+        // Given
+        when(input.read(any(byte[].class))).thenThrow(new IOException());
+
+        // When
+        copyAndClose(input, output);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void It_is_not_possible_to_copy_and_close_from_an_invalid_output() throws IOException {
+
+        final InputStream input = WEB_XML_JAR.openStream();
+        final OutputStream output = mock(OutputStream.class);
+
+        // Given
+        doThrow(new IOException()).when(output).write(any(byte[].class), any(Integer.class), any(Integer.class));
+
+        // When
+        copyAndClose(input, output);
+    }
+
+    @Test
+    public void It_is_possible_to_copy_a_jar_web_xml_to_the_filesystem() {
+
+        // Given
+        final int hash = new Random().nextInt();
+        final File output = tempWebXml(hash);
+
+        // When
+        final String webXml = copy(WEB_XML_JAR, output);
+
+        // Then
+        assertTrue(new File(webXml).exists());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void It_is_not_possible_to_copy_from_an_invalid_web_xml() throws MalformedURLException {
+
+        // Given
+        final int hash = new Random().nextInt();
+        final URL input = new URL("file:/invalid.jar!web.xml");
+        final File output = tempWebXml(hash);
+
+        // When
+        copy(input, output);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void It_is_not_possible_to_copy_to_an_invalid_web_xml() {
+
+        // Given
+        final File output = new File("invalid/web.xml");
+
+        // When
+        copy(WEB_XML_JAR, output);
+    }
+}
